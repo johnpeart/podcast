4291578713b0ac80e1cf086c427bc0fa4877f52b
John Peart <john.peart@me.com>
2020-03-08 23:17:20 +0000

    New header style

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,6 +1,5 @@
 window.onload = function() {
   checkJS();
-  popPodcastIcon();
 };
 
 function checkJS() {
@@ -13,14 +12,4 @@ function toggleMenu() {
   
   var menu = document.getElementById("header-menu-toggle-image");
   menu.classList.toggle("open");
-}
-
-function popPodcastIcon() {
-	var podcastsIcon = document.getElementById("podcasts-icon");
-	
-	if (podcastsIcon.classList.contains("start")) {
-    podcastsIcon.classList.remove("start");
-    podcastsIcon.classList.add("end");
-	}
-
 }
